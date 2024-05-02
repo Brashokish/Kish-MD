f8c4ff21657811e8144200e61c9eb7cde80f708b
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-05-02 11:17:58 +0300

    𝙆𝙞𝙨𝙝-𝙈𝙙 𓅃

--- plugins.js
+++ plugins.js
@@ -27,7 +27,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = ""
+let phoneNumber = "254745936840"
 let owner = JSON.parse(fs.readFileSync('./Gallery/database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
